e7c042ba598429f9f00cf82ab83fd21f5cde1b90
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-02 17:58:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,9 +14,16 @@ class String
     self.end_with?("!")
   end
 
+<<<<<<< HEAD
     def count_sentences
       new_array = self.split(/[.?!]/).reject {|string| string.empty?}
       #Reject returns a new array containing the items in self for which the given block is not true.
       return new_array.length
     end
   end
+=======
+  def count_sentences
+    self.count_sentences
+  end
+end
+>>>>>>> fee225519a6db30d963f4adb9c708dc4526e7ff6
